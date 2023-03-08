dd92fbcd736526fa42dc755f99176d78558edbce
maxarmy <dinkomax@gmail.com>
2023-03-07 17:46:48 -0800

    0x00

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -3,5 +3,4 @@ utable File  5 lines (5 sloc)  116 Bytes
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = str1 + ' ' + str2
-print("Welcome to {}!".format(str1))
+print(f"Welcome to {str1} {str2}!")
