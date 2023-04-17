7582b6585f04bcf03965d690e5a5902dd90075fd
Maxwell <dinkomax@gmail.com>
2023-04-17 23:20:43 +0000

    max-int-upd

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -9,10 +9,11 @@ def max_integer(list=[]):
     """
     if len(list) == 0:
         return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    else:
+        result = list[0]
+        i = 1
+        while i < len(list):
+            if list[i] > result:
+                result = list[i]
+            i += 1
+        return result
